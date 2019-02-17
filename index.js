57265a8614511bfb048cc7309fcc7acab5097e74
darckthekiller <47572769+darckthekiller@users.noreply.github.com>
2019-02-17 12:33:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const cfg require('./index.json');  
-const token = process.env.token
-const prefix = ("?");
-bot.on('ready', function () {
-    console.log("Je suis prêt à être utilisé.")
-    bot.user.setActivity('rien').catch(console.error)
-});
+var prefix = "?";
 
-
-bot.login(token);
+client.login("NTI5MjkwOTk3MDM4MDU1NDM0.D0rO4g.ADU9igeXAHe7LIsOgTd0hb6ew-4");
