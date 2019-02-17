276bc816c576a4a02b1f602a701cabcbdd105fce
darckthekiller <47572769+darckthekiller@users.noreply.github.com>
2019-02-17 12:11:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ bot.on('ready', function () {
 
 bot.on('guildMemberAdd', member => {
     member.createDM().then(channel => {
-        return channel.send('Bienvenue sur le serveur de Alexpgm' + member.displayName)
+        return channel.send('Bienvenue sur le serveur de darckthekiller' + member.displayName)
         console.log(`${member.displayName} à rejoind le serveur.`)
     }).catch(console.error)
 });
@@ -32,11 +32,6 @@ bot.on('message', msg => {
     }
     if (msg.content.match(/salut/i)) {
             msg.reply('Je suis d\'accord avec toi.')
-    }
-    if (msg.content === prefix + "site"){
-        msg.channel.send("https://alexpgm.000webhostapp.com/")
-        console.log("Une personne a demandé pour aller sur ton site.")
-    }
 
 });
 
